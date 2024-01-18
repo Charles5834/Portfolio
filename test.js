5b6ab2bb6fd7775e384b1563d7bf27e6069e01c7
Charles5834 <156222884+Charles5834@users.noreply.github.com>
2024-01-18 12:52:34 -0600

    Charles Oroceo 1/18/2024

---
+++ test.js
@@ -0,0 +1,10 @@
+var ii = alert("Charles Oroceo 1/17/2024");
+var i = prompt("give number and it'll subtract 3"); 
+i -= 3;
+var iii = prompt ("What is your favorite video game?");
+iii = ("your favorte video game is:  " + iii);
+window.alert(i);
+window.alert(iii);
+const myObj = {variable:"the variables are i, ii, iii ", value:"the valve are the input answers"};
+console.log(myObj);
+
